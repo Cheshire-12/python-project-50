06a39565c9bb84f565b128875c0c7e73b5bc60c0
Andrey <andr.vnk@gmail.com>
2026-02-02 21:17:16 +0300

    Шаг 3 выполнен

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -1,11 +1,9 @@
 import argparse
 
-def generate_diff():
+def get_parser():
     parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    args = parser.parse_args()
-    print(args.first_file, args.second_file)
-
-if __name__ == '__main__':
-    generate_diff()
+    parser.add_argument('-f', '--format', help='set format of output', default='stylish')
+    
+    return parser
---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,11 @@
+from gendiff.parser import get_parser
+from gendiff.engine import genarate_diff
+
+def main():
+    parser = get_parser()
+    args = parser.parse_args()
+    result = genarate_diff(args.first_file, args.second_file)
+    print(result)
+
+if __name__ == '__main__':
+    main()
